97c32c7976d14ade347e2a86f280ee9bebb9d734
Coahle Aub <aubcoahle123@gmail.com>
2026-09-01 13:15:52 +0200

    Add files via upload

---
+++ 4.3 EJS Partials/index.js
@@ -0,0 +1,24 @@
+import express from "express";
+
+const app = express();
+const port = 3000;
+
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+app.use(express.static("public"));
+
+app.get("/about", (req, res) => {
+  res.render("about");
+});
+
+app.get("/contact", (req, res) => {
+  res.render("contact");
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
